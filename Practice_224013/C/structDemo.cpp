e3cac618f5844f96a8494e6244d1461f01ae1fa3
trankienduc401205 <trankienduc401205@gmail.com>
2024-10-27 11:45:07 +0700

    Duc chinh sua

--- Practice_224013/C/structDemo.cpp
+++ Practice_224013/C/structDemo.cpp
@@ -4,7 +4,7 @@
 //tạo ra cấu trúc dữ liệu struct employee
 struct employee {
     int empId;
-    char empName[200]; // change the salary by requirement #1234
+    char empName[250]; // change the salary by requirement #1234
     int salary;
 };
 
