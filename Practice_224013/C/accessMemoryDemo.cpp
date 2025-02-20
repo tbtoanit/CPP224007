4b3c233eda17b9623d4ee7009ef325bb70d3cac3
hoaqha <hoanghatsn@gmail.com>
2025-02-20 20:33:39 +0700

    genius

--- Practice_224013/C/accessMemoryDemo.cpp
+++ Practice_224013/C/accessMemoryDemo.cpp
@@ -7,18 +7,12 @@
 
 int main() {
     int z = 1;
-    int x = 10;
-	int y = 20;
-    int *p = &x; //biến p được gọi là pointer/cursor/con trỏ
 
-    printf("%p", p);
-
-    printf("Change something here");
     printf("HuynhCuNhan");
-	printf("done");
-    printf("done");
-    printf("done");printf("done");
 
     printf("hello");
+
     printf("i am nhat") ;
+
+    printf("Pham. Hoang` Ha`");
 }
