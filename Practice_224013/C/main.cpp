e64beb8da26d9f29620268527562967a45b98c22
PhungVanQuan <quanphung3102005@gmail.com>
2026-02-01 19:17:02 +0700

    Phung Van Quan

--- Practice_224013/C/main.cpp
+++ Practice_224013/C/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
 	int y= 0;// xin chao day la cmt cua Dinh
 
     std::cout << "Ey yo, what's up, bruh!" << std::endl;
     // std::cout << "Hello, Thanh da o day" << std::endl;
+    int m=15;
+    cout<<m;
     return 0;
 }
